4529dbf6dbf6509c921fd58f93d729b1b6a62c37
Vien Vu <lamvienbk@gmail.com>
2016-02-01 17:21:05 +0700

    First Commit

---
+++ DemoSubClass/ViewController2.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController2.swift
+//  DemoSubClass
+//
+//  Created by Vien Vu  on 2/1/16.
+//  Copyright © 2016 VienVu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
